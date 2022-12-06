82f356c0958f360d4271d321e7843d378fc65e53
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2022-12-06 15:48:54 -0800

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
-import threading
-import webbrowser
 from myapp import myapp_obj, db
 
 DEBUG = False
-PORT_NUMBER = 5098
+PORT_NUMBER = 5099
+
+def launch_browser():
+    webbrowser.open(f'http://localhost:{PORT_NUMBER}', new=0)
+
 
 # Create *.db file from schema (if doesn't exists)
 try:
@@ -12,7 +13,12 @@ try:
 except:
     pass
 
+# Disabled autolaunch browser to deploy heroku
+#if not DEBUG:
+#    threading.Timer(1, launch_browser).start()
 
 # Run flask app server
 
 myapp_obj.run(debug=DEBUG, port=PORT_NUMBER)
+if __name__ == '__main__':
+    myapp_obj.run(debug=DEBUG)
